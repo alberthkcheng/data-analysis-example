3883aedbb3d5adf439c434d6cf06def295ec5e86
Albert HK Cheng <albert.hk.cheng@gmail.com>
2018-03-30 20:37:29 +0800

    Add R Solution for market basket analysis

---
+++ market_basket_analysis.R
@@ -0,0 +1,20 @@
+# Market Basket Analysis
+# Reference: https://towardsdatascience.com/a-gentle-introduction-on-market-basket-analysis-association-rules-fa4b986a40ce
+
+library(data.table)
+library(arules)
+
+orders <- fread("input/SampleOrders.csv")
+# Make sure OrderID x ProductID is unique
+orders <- orders[,.(Quantity=sum(Quantity)), by=.(OrderID, ProductID)]
+
+# Compute bestseller
+bestseller <- orders[,.(count=.N), by=.(ProductID)]
+setorder(bestseller, -count)
+# There are 7 products with 14 purchases, for simplicity, we randomly pick 3 as the top 8th, 9th,10th
+bestseller <- head(bestseller, 10) 
+
+# Compute supp, conf, lift
+trans <- as(split(factor(orders[["ProductID"]]), orders[["OrderID"]]), "transactions")
+rules <- apriori(trans, parameter = list(supp = 0.0002, conf = 0.4, minlen=2,target = "rules"))
+inspect(head(rules, by = "lift"))
