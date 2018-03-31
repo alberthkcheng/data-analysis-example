2d2b7ec968141daa09d1b929f16cf10d1923eab5
Albert HK Cheng <albert.hk.cheng@gmail.com>
2018-03-31 20:10:32 +0800

    Add Test case and comments for unique pageview

--- unique_pageview.R
+++ unique_pageview.R
@@ -1,32 +1,47 @@
-# R Solution to find the number of unique pageviews per day
+# SQL solution tested with Spark
+library(sparklyr)
+library(tidyverse)
+library(data.table)
+library(DBI)
+
+# Simple ETL to solve two issues:
+# 1. transform Visit_Time to timestamp due to absence of time data type for Spark
+# 2. There is no duplicated pageview in original dataset which is a testcase we want to add
+
+# Extract
+# sample_pageview <- read_csv("input/SamplePageviews.csv")
+# 
+# # Transform
+# sample_pageview <- sample_pageview %>% 
+#   union(data_frame(ID=13L,User_ID=1L,Page_ID=54L, Visit_Date=as.Date("2018-01-01"), Visit_Time=hms::as.hms("11:56:12"))) %>%
+#   mutate(
+#     Visit_Time = lubridate::ymd_hms(str_c(Visit_Date, Visit_Time, sep = " "))) 
+# # Saved as CSV for future use
+# write_csv(sample_pageview,  "input/SamplePageviews_transformed.csv")
+
+# Load
+sc <- spark_connect(master = "local")
+sample_pageview <- read_csv("input/SamplePageviews_transformed.csv", 
+                            col_types = cols(Visit_Date = col_character()))
+copy_to(sc, sample_pageview, overwrite = TRUE)
+
+# Execute SQL Query
+dbGetQuery(sc, read_file("unique_pageview.sql")) %>%
+  write_csv("output/unique_pageview.csv")
+
+# R Solution for reference
+# 
 # library(tidyverse)
 # library(data.table)
-# 
-# sample_pageview <- fread("input/SamplePageviews.csv")
-# setorder(sample_pageview, User_ID, Visit_Date, Visit_Time)
-# sample_pageview[,Visit_Time:=hms::parse_hms(Visit_Time)]
-# 
+# library(lubridate)
 # # Determine new session
-# sample_pageview[,time_since_last_activity:=Visit_Time-shift(Visit_Time), by=.(User_ID, Visit_Date)]
+# sample_pageview <- fread("input/SamplePageviews_transformed.csv")
+# setorder(sample_pageview, User_ID, Visit_Date, Visit_Time)
+# sample_pageview[,Visit_Time:=ymd_hms(Visit_Time)]
+# sample_pageview[,time_since_last_activity:=difftime(Visit_Time, shift(Visit_Time), units="secs"), by=.(User_ID, Visit_Date)]
 # sample_pageview[,is_new_session:=as.integer(is.na(time_since_last_activity)|time_since_last_activity>10*60)]
 # sample_pageview[, session_id := cumsum(is_new_session)]
 # # Compute Result
 # unique_pageview <- sample_pageview[,.(Total_User_Sessions = length(unique(session_id))), by=.(Page_ID, Visit_Date)]
 # setorder(unique_pageview, Page_ID, Visit_Date)
-# 
 # unique_pageview %>% write_csv("output/unique_pageview.csv")
-
-
-# SQL solution tested with Spark
-library(sparklyr)
-library(tidyverse)
-library(DBI)
-sc <- spark_connect(master = "local")
-sample_pageview <- read_csv("input/SamplePageviews.csv", col_types = cols(.default = col_character()))
-sample_pageview <- sample_pageview %>% 
-  mutate(
-    Visit_Time = lubridate::ymd_hms(str_c(Visit_Date, Visit_Time, sep = " ")))
-sample_pageview <- copy_to(sc, sample_pageview, overwrite = TRUE)
-dbGetQuery(sc, read_file("unique_pageview.sql"))
-
-# Problem - there is no duplicated pageview - have to add a test case
--- unique_pageview.sql
+++ unique_pageview.sql
@@ -1,7 +1,8 @@
--- page_view_with_session Compute Session_ID
+-- page_view_with_session compute Session_ID
 WITH page_view_with_session AS(
   SELECT *, 
-    SUM(cast(Seconds_Since_Last_Pageview IS NULL OR Seconds_Since_Last_Pageview > 60*10 AS INTEGER))
+    -- It is a new session if it is user's 1st activity or last activity is more than 10 mins ago.
+    SUM(CAST(Seconds_Since_Last_Pageview IS NULL OR Seconds_Since_Last_Pageview > 60*10 AS INTEGER))
     OVER (ORDER BY User_ID, Visit_Time)
     AS Session_ID 
   FROM (
@@ -14,7 +15,8 @@ WITH page_view_with_session AS(
     FROM sample_pageview
   )
 )
--- DISTINCT IS USED So that it is countint unique pageview instead of total pageview
+-- SELECT * FROM page_view_with_session LIMIT 100 -- Uncomment to test page_view_with_session table
+-- DISTINCT is used so that it is counting unique pageview instead of total pageview
 SELECT Page_ID, Visit_Date, COUNT(DISTINCT Session_ID) AS Total_User_Sessions
 FROM page_view_with_session
 GROUP BY Page_ID, Visit_Date
